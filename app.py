40065fd7a7cf54037553246544954fc4f68e4ccd
Dutchie1990 <danielboone1990@gmail.com>
2021-03-22 19:43:06 +0000

    delete task functionality

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 from flask_pymongo import PyMongo
-from flask import (Flask, flash, render_template, 
-                    redirect, request, session, url_for)
+from flask import (Flask, flash, render_template,
+                redirect, request, session, url_for)
 from bson.objectid import ObjectId
 from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
@@ -136,6 +136,13 @@ def edit_task(task_id):
     task = mongo.db.tasks.find_one({"_id": ObjectId(task_id)})
     categories = mongo.db.categories.find().sort("category_name", 1)
     return render_template("edit_task.html", task=task, categories=categories)
+
+
+@app.route("/delete_task/<task_id>")
+def delete_task(task_id):
+    mongo.db.tasks.remove({"_id": ObjectId(task_id)})
+    flash("Task succesfully deleted")
+    return redirect(url_for('get_tasks'))
 
 
 if __name__ == "__main__":
